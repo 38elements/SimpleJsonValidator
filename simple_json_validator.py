9e90174f30852e011277e54998e94338ff4d9580
Hiro <mh19820223@gmail.com>
2013-07-16 17:27:16 +0900

    add schema

--- simple_json_validator.py
+++ simple_json_validator.py
@@ -1,24 +1,69 @@
 # coding: utf-8
-
+import pprint
 
 class SimpleJsonValidator(object):
 
     def __init__(self, schema):
-        self.check_schema(schema)
         self.__schema = schema
-        self.__types = (int, long, float, bool, str, unicode, list, dict)
+        self.__types = (int, float, bool, str)
+        self.__target_types = (int, long, float, bool, str, unicode)
+        self.__structures = (list, dict)
+        self.check_schema(schema)
 
-    def check_schema(schema):
-        pass
+    def is_number(self, data):
+        return isinstance(data, (int, long)) and not isinstance(data, bool)
 
-    def validate(self, data):
-        pass
+    def is_float(self, data):
+        return isinstance(data, float)
+
+    def is_bool(self, data):
+        return isinstance(data, bool)
+
+    def is_string(self, data):
+        return isinstance(data, (str, unicode))
+
+    def is_list(self, data):
+        return isinstance(data, list)
+
+    def is_dict(self, data):
+        return isinstance(data, dict)
+
+    def check_schema_dict(self, dict_data, path):
+        for key in dict_data:
+            if not self.is_string(key) and len(key) > 0:
+                raise SchemaError("InValid key: %s is not string. path=%s" % (key, path))
+        return True
+
+    def check_schema_list(self, list_data, path):
+        if not len(list_data) == 1:
+            raise SchemaError("InValid lengh: list len is not 1. path=%s" % (path,))
+        return True
+
+    def check_schema(self, data, key="", path=""):
+        key = str(key)
+        path += "." + key
+        if not isinstance(data, self.__structures) and not data in self.__types:
+            raise SchemaError("InValid type at key='%s' path='%s'.  schema is %s" % (key, path, pprint.pformat(data)))
+        if self.is_dict(data):
+            self.check_schema_dict(data, path)
+            for target_key, target_data in data.iteritems():
+                self.check_schema(target_data, target_key, path)
+        elif self.is_list(data):
+            self.check_schema_list(data, path)
+            self.check_schema(data[0], "[0]", path)
+        return True
+
+        
+
+    def validate(self, data, schema=None):
+        schema = self.schema if schema == None else schema
 
 
 
-class ValidationError(Error):
+
+class ValidationError(Exception):
     pass
 
 
-class SchemaError(Error):
+class SchemaError(Exception):
     pass
--- test.py
+++ test.py
@@ -1,13 +1,41 @@
 # coding: utf-8
 
-from unittest import TestCase
-
+from unittest import TestCase, main
+from simple_json_validator import SimpleJsonValidator, SchemaError, ValidationError
 
 class SchemaTest(TestCase):
 
     def setUp(self):
         pass
 
+    def test_schema_type(self):
+        self.assertRaises((SchemaError), SimpleJsonValidator, unicode)
+
+    def test_check_schema_dict(self):
+        sjv = SimpleJsonValidator(str)
+        self.assertRaises((SchemaError), sjv.check_schema_dict, {(1,2): "1"}, "")
+        self.assertTrue(sjv.check_schema_dict({"d": 1}, ""))
+        self.assertTrue(sjv.check_schema_dict({u"d": 1}, ""))
+
+    def test_check_schema_list(self):
+        sjv = SimpleJsonValidator(str)
+        self.assertRaises((SchemaError), sjv.check_schema_list, [int, int], "")
+        self.assertTrue(sjv.check_schema_list([int], ""))
+
+    def test_check_schema(self):
+        SimpleJsonValidator([int])
+        SimpleJsonValidator({"foo":int})
+        SimpleJsonValidator({
+                                "foo":int,
+                                "bar": [str],
+                                "hoge": {
+                                        "foo": {
+                                            "foo": str,
+                                            "bar": int,
+                                        },
+                                        "bar": [str]
+                                    }
+                                })
 
 class ValidationTest(TestCase):
 
@@ -16,4 +44,4 @@ class ValidationTest(TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
